57be8e046a384082d27a7454e83f1536c7b5f429
Kaleab007 <abatenehkaleab@gmail.com>
2022-05-17 22:46:48 -0700

    fixed

--- components/TopBar.js
+++ components/TopBar.js
@@ -2,13 +2,15 @@ import NavBar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Button'
 import Button from 'react-bootstrap/Nav'
 
-function TopBar
+function TopBar () {
 
 
-return
+return 
+ (
+    <div>
 <Navbar bg="light" expand="lg" >
-    <Navbar.Brand style={{margin: "5px 0 0 20px"}} href="#home">
-        <img src="[Instacart logo src]" /> 
+    <Navbar.Brand style={{margin: "5px 0 0 20px"}} href="#home"><img src="https://d2guulkeunn7d8.cloudfront.net/assets/beetstrap/brand/instacart-logo-color-4db9d81ca0b7638befdc4bd331f64a2633df790c0b55ef627c99b1ba77af72b7.svg"></img>
+        
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
@@ -20,4 +22,6 @@ return
         </Nav>
     </Navbar.Collapse>
 </Navbar>
-
+</div>
+)}
+export default TopBar 
